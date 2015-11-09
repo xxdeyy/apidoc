cb3ab8edcdc06b502be431da7092faa2b22c4d9e
xxdeyy <xxdeyy@gmail.com>
2015-11-09 13:01:17 +0800

    没啥特别的

--- src/main/java/com/f2boy/Environment.java
+++ src/main/java/com/f2boy/Environment.java
@@ -1,8 +1,11 @@
 package com.f2boy;
 
+import org.apache.commons.lang.math.RandomUtils;
+
 public class Environment {
 
-    private static final String SYS_VERSION = "20991231";
+//    private static final String SYS_VERSION = "20991231";
+    private static final String SYS_VERSION = RandomUtils.nextInt(10000) + "";
 
     public String getSysVersion() {
 
